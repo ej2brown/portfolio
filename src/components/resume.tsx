fa6a48dca649c9e031c54a667d363db9c0a38d06
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-05 19:23:05 -0400

    update resume format sections

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -5,16 +5,32 @@ export default class Resume extends Component {
   render() {
     return (
       <section id="resume" >
+        <h1>Resume</h1>
 
-        <h2>Resume</h2>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum."
-            </p>
+        <section className="eduation">
+          <h2>Education</h2>
+          <h4>Lighthouse Labs | Mar - May 2020 </h4>
+          <div>Diploma: Full-Stack Web Development</div>
+          <img src="/images/lighthouselabs-logo.png" alt="lighthouselabs-logo" width="150" height="50"></img>
+
+          <h4>University of Waterloo | 2015 - 2019</h4>
+          <div>Bachelor of Science: General</div>
+          <img src="/images/uwaterloo-logo.jpg" alt="uwaterloo-logo" width="200" height="100"/>
+
+        </section>
+        <h2>Work Experience</h2>
+        <h4>Lorem ipsum</h4>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+          <li>Aliquam tincidunt mauris eu risus.</li>
+          <li>Vestibulum auctor dapibus neque.</li>
+        </ul>
+        <h4>Lorem ipsum</h4>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+          <li>Aliquam tincidunt mauris eu risus.</li>
+          <li>Vestibulum auctor dapibus neque.</li>
+        </ul>
       </section >
     )
   }
