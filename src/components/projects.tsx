fdadee8db5b2f755dbcb34e87c4ae89258ce1790
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-05 19:21:26 -0400

    init projects section

---
+++ src/components/projects.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Projects extends Component {
+
+  render() {
+    return (
+      <section id="projects" >
+        <h1>Projects</h1>
+
+        <h4>Lorem ipsum</h4>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <img src="img.jpg" alt="project-preview" width="500" height="600"/>
+        <a href="/#">link to project</a>
+
+        <h4>Lorem ipsum</h4>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <img src="img.jpg" alt="project-preview" width="500" height="600"/>
+        <a href="/#">link to project</a>
+
+        <h4>Lorem ipsum</h4>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <img src="img.jpg" alt="project-preview" width="500" height="600"/>
+        <a href="/#">link to project</a>
+
+        <h4>Lorem ipsum</h4>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <img src="img.jpg" alt="project-preview" width="500" height="600"/>
+        <a href="/#">link to project</a>
+      </section >
+    )
+  }
+}
