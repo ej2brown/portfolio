07b01117a3faf651f7ccc38d9a8e93c4a0638a45
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-05 19:22:28 -0400

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,11 +7,9 @@ export default class Header extends Component {
           <li className="current"><a className="" href="#home">Home</a></li>
           <li><a className="" href="#about">About</a></li>
           <li><a className="" href="#resume">Resume</a></li>
-          <li><a className="" href="#portfolio">Works</a></li>
-          <li><a className="" href="#testimonials">Testimonials</a></li>
+          <li><a className="" href="#projects">Projects</a></li>
           <li><a className="" href="#contact">Contact</a></li>
         </ul>
-          <a className="scrolldown" href="#about"><i className="fa fa-arrow-circle-down fa-4x"/></a>
       </header>
     )
   }
