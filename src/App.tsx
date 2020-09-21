a53622b5dff24debbb2c2f72f18064b64cd6d1ed
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-21 17:59:28 -0600

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import Header from './components/header';
+import Welcome from './components/welcome';
 import About from './components/about';
-import Resume from './components/resume';
 import Projects from './components/projects';
-// import Stepper from './components/materialui/stepper';
 
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-class App extends Component {
-  render() {
-    return (
+export default function App() {
+  return (
+    <Router>
+      <CssBaseline />
       <div className="App">
         <Header />
-        {/* <Stepper/> */}
-        <About />
-        <Resume />
-        <Projects/>
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 };
-
-export default App;
