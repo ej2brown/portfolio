74e0f8c053f5a4a77362c494f46ff04994d5dec9
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-21 17:59:10 -0600

    Add welcome message

---
+++ src/components/welcome.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section id="welcome" className="fflex-container">
+      <h3 className="welcome-txt">Welcome! I'm a web developer currently living in Toronto.</h3>
+      <a className="btn" href="/about">Learn More</a>
+    </section>
+  );
+};
