ca984157e6b525a358dcedcd841f1d3d14e102a3
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-21 18:03:26 -0600

    Add drawer menu for small screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,169 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1 className="site-heading">
-          <div className="site-heading-upper">Elizabeth Brown</div>
-          <div className="site-heading-lower">Full-stack Developer</div>
-        </h1>
-        <ul id="nav" className="nav">
-          <li className="current"><a className="" href="#home">Home</a></li>
-          <li><a className="" href="#about">About</a></li>
-          <li><a className="" href="../images/resume.pdf">Resume</a></li>
-          <li><a className="" href="#projects">Projects</a></li>
-          <li><a className="" href="#contact">Contact</a></li>
+// @material-ui/core components
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+// @material-ui/icons
+const drawerWidth = 140;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      // minHeight: 100
+    },
+    transparent: {
+      "&,&:focus,&:hover,&:visited": {
+        color: "inherit",
+        background: "transparent",
+        boxShadow: "none"
+      }
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  }),
+);
+
+export default function HeaderNav() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const menu = [
+    { page: 'Home', href: '/' },
+    { page: 'About', href: '/about' },
+    { page: 'Resume', href: '../images/resume.pdf' },
+    { page: 'Projects', href: '/projects' },
+    { page: 'Contact', href: '/contact' }
+  ];
+
+  return (
+    <div id="navbar" className={classes.root}>
+      <AppBar
+        id="navbarSmall"
+        position="fixed"
+        className={clsx(classes.appBar, classes.transparent, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {menu.map((text, index) => (
+            <ListItem button key={index} component="a" href={text.href}>
+              <ListItemText primary={text.page} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <header id="navbarLarge">
+        <ul id="navList" className="nav">
+          <li className="current"><a className="" href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="../images/resume.pdf">Resume</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </header>
-    )
-  }
+      <div className="site-heading">
+        <h1 className="site-heading-upper">Elizabeth Brown</h1>
+        <h2 className="site-heading-lower">Full-stack Developer</h2>
+      </div>
+    </div>
+  )
 };
