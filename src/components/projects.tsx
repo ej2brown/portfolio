46b128231f464905ea19e65de879100540658c56
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-21 18:00:31 -0600

    Add key for components

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,10 +6,11 @@ export default class Projects extends Component {
 
   render() {
     return (
+      
       <section id="projects" >
         <h1>Projects</h1>
-        {projects.map((project: { title: string; paragraph: string; link: string; img: string; frontEnd: string; backEnd: string; date: string; highlight: string;}) => {
-          return <Card title={project.title} paragraph={project.paragraph} link={project.link} img={project.img} frontEnd={project.frontEnd} backEnd={project.backEnd} date={project.date} highlight={project.highlight}></Card>
+        {projects.map((project: { title: string; paragraph: string; link: string; img: string; frontEnd: string; backEnd: string; date: string; highlight: string;}, index: number) => {
+          return <Card key={index} title={project.title} paragraph={project.paragraph} link={project.link} img={project.img} frontEnd={project.frontEnd} backEnd={project.backEnd} date={project.date} highlight={project.highlight}></Card>
         })}
       </section >
     )
